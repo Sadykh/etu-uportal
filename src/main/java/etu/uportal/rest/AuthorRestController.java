90209aeac584b9d272d6549b17649fe14e82e5f7
Sadykh <sadykh.sadykhov@ya.ru>
2019-06-13 00:31:28 +0300

    Fix rest author dto

--- src/main/java/etu/uportal/rest/AuthorRestController.java
+++ src/main/java/etu/uportal/rest/AuthorRestController.java
@@ -1,17 +1,22 @@
 package etu.uportal.rest;
 
 import etu.uportal.domain.author.Author;
+import etu.uportal.domain.publication.Publication;
 import etu.uportal.infrastructure.service.AuthorService;
 import etu.uportal.spring.OffsetLimitPageable;
 import etu.uportal.web.dto.author.AuthorCreateDto;
+import etu.uportal.web.dto.publication.PublicationCreateDto;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @RestController
@@ -25,8 +30,13 @@ public class AuthorRestController {
     @ApiOperation(value = "Получить список всех авторов")
     @PreAuthorize("permitAll()")
     @GetMapping("/")
-    public Page<Author> getUser(OffsetLimitPageable pageRequest) {
-        return authorService.getAll(pageRequest);
+    public Page<AuthorCreateDto> getUser(OffsetLimitPageable pageRequest) {
+        Page<Author> items = authorService.getAll(pageRequest);
+        List<AuthorCreateDto> result = new ArrayList<>();
+        items.getContent().forEach(item -> {
+            result.add(new AuthorCreateDto(item.getId(), item.getFirstName(), item.getLastName(), item.getMiddleName(), item.getFirstNameEn(), item.getLastNameEn(), item.getMiddleNameEn(), item.getAuthorFields()));
+        });
+        return new PageImpl<>(result);
     }
 
     @ApiOperation(value = "Добавить нового автора")
