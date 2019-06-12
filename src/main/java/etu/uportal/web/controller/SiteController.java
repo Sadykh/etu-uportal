8df2ac01621b944312b0590611f4b8ba72ed11c2
Sadykh <sadykh.sadykhov@ya.ru>
2019-06-12 17:00:37 +0300

    Fix swagger UI and disable users

--- src/main/java/etu/uportal/web/controller/SiteController.java
+++ src/main/java/etu/uportal/web/controller/SiteController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class SiteController {
 
-    @RequestMapping("*")
+    @RequestMapping("/")
     public String index() {
         return "redirect:/publication/";
 
