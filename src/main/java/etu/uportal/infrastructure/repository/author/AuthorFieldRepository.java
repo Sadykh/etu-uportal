b9e11cd264e5600056085352906fe462d0037ffb
Sadykh <sadykh.sadykhov@ya.ru>
2019-06-12 22:48:32 +0300

    May be fix AuthorField repository

--- src/main/java/etu/uportal/infrastructure/repository/author/AuthorFieldRepository.java
+++ src/main/java/etu/uportal/infrastructure/repository/author/AuthorFieldRepository.java
@@ -3,11 +3,12 @@ package etu.uportal.infrastructure.repository.author;
 import etu.uportal.domain.author.Author;
 import etu.uportal.domain.author.AuthorField;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
 
 public interface AuthorFieldRepository extends JpaRepository<AuthorField, Long> {
 
-//    @Transactional
-//    void deleteAuthorFieldsByAuthor(Author author);
+    @Transactional
+    void deleteAuthorFieldsByAuthor(Author author);
 }
