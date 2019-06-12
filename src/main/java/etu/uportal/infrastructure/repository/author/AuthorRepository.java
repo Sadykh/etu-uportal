09c63f3c5c83b828062fe8bb5a764f17353dfd0a
Sadykh <sadykh.sadykhov@ya.ru>
2019-06-12 23:56:31 +0300

    Add repository annotations

--- src/main/java/etu/uportal/infrastructure/repository/author/AuthorRepository.java
+++ src/main/java/etu/uportal/infrastructure/repository/author/AuthorRepository.java
@@ -2,7 +2,9 @@ package etu.uportal.infrastructure.repository.author;
 
 import etu.uportal.domain.author.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AuthorRepository extends JpaRepository<Author, Long> {
 
 }
