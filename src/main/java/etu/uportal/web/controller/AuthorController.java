5f75608c311e8be6a1f935c270d2d29ae50c7d6d
Sadykh <sadykh.sadykhov@ya.ru>
2019-06-17 16:39:12 +0300

    Add method for view author

--- src/main/java/etu/uportal/web/controller/AuthorController.java
+++ src/main/java/etu/uportal/web/controller/AuthorController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -39,5 +40,14 @@ public class AuthorController {
         model.addAttribute("authors", authors);
         model.addAttribute("title", "Список авторов");
         return "author/index";
+    }
+
+    @GetMapping("/view/{id}")
+    public String index(Model model, @PathVariable Long id) {
+        Author author = authorService.getOneById(id);
+        model.addAttribute("author", author);
+        model.addAttribute("title", "Просмотр автора");
+        model.addAttribute("publications", publicationService.getPublicationsByAuthor(author));
+        return "author/view";
     }
 }
